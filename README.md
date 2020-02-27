a7f9e831d35ee1a0c3ebfdce403334f84e4ff409
tohashi <dots64if@gmail.com>
2020-02-27 14:14:57 +0900

    feat: setup packages

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # eslint-config-freee-flowtype
+
 Shared configuration for JavaScript with flowtype in freee.
+
+## Installation
+
+```sh
+# npm
+npm install eslint-config-freee-flowtype -D -E
+
+# yarn
+yarn add eslint-config-freee-flowtype -D -E
+```
+
+Then add it to your `.eslintrc.js`
+
+```js
+module.exprots = {
+  extends: ['eslint-config-freee-flowtype']
+};
+```
---
+++ index.js
@@ -0,0 +1,35 @@
+module.exports = {
+  parser: 'babel-eslint',
+  plugins: ['flowtype', 'react', 'react-hooks', 'jsx-a11y', 'import'],
+  extends: [
+    'eslint:recommended',
+    'plugin:flowtype/recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier/react',
+    'prettier/@typescript-eslint',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript'
+  ],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: true,
+        trailingComma: 'none',
+        jsxBracketSameLine: false
+      }
+    ],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
+  },
+  settings: {
+    'import/resolver': 'webpack',
+    react: { version: 'detect' }
+  }
+};
